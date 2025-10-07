592437a7181515e1af96b8067c06d5af56ade021
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-07 20:59:48 -0300

    Resolvendo o 'desafio041-Classificando_Atletas'

--- desafios/desafio041-Classificando_Atletas.py
+++ desafios/desafio041-Classificando_Atletas.py
@@ -1,1 +1,14 @@
-# O arquivo './desafios/desafio041-Classificando_Atletas.py' foi criado automaticamente
+from datetime import date
+ano_atual = date.today().year
+ano_nascimento = int(input('Digite o ano do seu nascimento (Formato AAAA): '))
+idade = ano_atual - ano_nascimento
+if idade <= 9:
+    print('MIRIM')
+elif 9 < idade <= 14:
+    print('INFANTIL')
+elif 14 < idade <= 19:
+    print('JUNIOR')
+elif 19 < idade <= 20:
+    print('SÊNIOR')
+else:
+    print('MASTER')
