9c222fde416a635f35ff59e81055839680c14949
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-07 20:41:22 -0300

    Resolvendo o 'desafio040-Aquele_classico_da_Media'

--- desafios/desafio040-Aquele_clássico_da_Média.py
+++ desafios/desafio040-Aquele_clássico_da_Média.py
@@ -1,1 +1,11 @@
-# O arquivo './desafios/desafio040-Aquele_clássico_da_Média.py' foi criado automaticamente
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('MÃ©dia = {:.2f}'.format(media))
+if media < 5:
+    print('REPROVADO')
+#elif media >= 5 and media < 7:
+elif 5 <= media < 7:
+    print('RECUPERAÃ‡ÃƒO')
+else:
+    print('APROVADO')
