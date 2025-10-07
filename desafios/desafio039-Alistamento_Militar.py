cee2d6c0e1284fd704d7e193be1b06971174eb16
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-07 20:27:58 -0300

    Resolvendo o 'desafio039-Alistamento_Militar'

--- desafios/desafio039-Alistamento_Militar.py
+++ desafios/desafio039-Alistamento_Militar.py
@@ -1,1 +1,14 @@
-# O arquivo './desafios/desafio039-Alistamento_Militar.py' foi criado automaticamente
+from datetime import date
+ano_nascimento = int(input('Por favor, digite o ano de seu nascimento (Formato AAAA): '))
+ano_atual = date.today().year
+idade = ano_atual - ano_nascimento
+if idade < 18:
+    print('Ainda vai se alistar.')
+    tempo_resta = 18 - idade
+    print('Ainda faltam {} ano(s) para se alistar'.format(tempo_resta))
+elif idade == 18:
+    print('É hora de se alistar!')
+else:
+    print('Já passou do tempo do ALISTAMENTO!')
+    tempo_passou = idade - 18
+    print('Você está {} ano(s) atrasado!'.format(tempo_passou))
