0dbc88ff2783a2480707b9210776d0f3126fb82f
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-08 09:18:07 -0300

    Atualizando o 'desafio039-Alistamento_Militar-EXTRA'

--- desafios/desafio039-Alistamento_Militar-EXTRA.py
+++ desafios/desafio039-Alistamento_Militar-EXTRA.py
@@ -1,17 +1,27 @@
 from datetime import date
-ano_nascimento = int(input('Por favor, digite o ano de seu nascimento (Formato AAAA): '))
-ano_atual = date.today().year
-idade = ano_atual - ano_nascimento
-idade_correta = 18
-print('Quem nasceu em {} tem {} anos em {}.'.format(ano_nascimento, idade, ano_atual))
-if idade < idade_correta:
-    saldo = idade_correta - idade
-    print('Ainda faltam {} ano(s) para o alistamento.'.format(saldo))
-    print('Seu alistamento será em {}.'.format(ano_atual + saldo))
-elif idade == idade_correta:
-    print('Você tem que se alistar IMEDIATAMENTE!')
+print('''Escolha o seu gênero: 
+[ 1 ] Masculino
+[ 2 ] Feminino''')
+genero = int(input('Sua opção: '))
+if genero ==  1:
+    ano_atual = date.today().year
+    idade_correta = 18
+    print('ALISTAMENTO OBRIGATÓRIO!')
+    ano_nascimento = int(input('Ano de nascimento (Formato AAAA): '))
+    idade = ano_atual - ano_nascimento
+    print('Quem nasceu em {} tem {} anos em {}.'.format(ano_nascimento, idade, ano_atual))
+    if idade < idade_correta:
+        saldo = idade_correta - idade
+        print('Ainda faltam {} ano(s) para o alistamento.'.format(saldo))
+        print('Seu alistamento será em {}.'.format(ano_atual + saldo))
+    elif idade == idade_correta:
+        print('Você tem que se alistar IMEDIATAMENTE!')
+    else:
+        saldo = idade - idade_correta
+        print('Você já deveria ter se alistado há {} ano(s).'.format(saldo))
+        print('Seu alistamento foi em {}.'.format(ano_atual - saldo))
+elif genero == 2:
+    print('No Brasil, o alistamento de mulheres é VOLUNTÁRIO e NÃO OBRIGATÓRIO!')
 else:
-    saldo = idade - idade_correta
-    print('Você já deveria ter se alistado há {} ano(s).'.format(saldo))
-    print('Seu alistamento foi em {}.'.format(ano_atual - saldo))
+    print('Opção inválida!')
 
