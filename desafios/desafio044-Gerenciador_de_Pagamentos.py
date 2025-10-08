a3e381e9ddea89747b5484d80832e26cc8b1ea66
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-07 22:19:08 -0300

    Resolvendo o 'desafio044-Gerenciador_de_Pagamentos'

--- desafios/desafio044-Gerenciador_de_Pagamentos.py
+++ desafios/desafio044-Gerenciador_de_Pagamentos.py
@@ -1,1 +1,21 @@
-# O arquivo './desafios/desafio044-Gerenciador_de_Pagamentos.py' foi criado automaticamente
+preco_normal = float(input('Digite o preço normal do produto: R$ '))
+print('Escolha uma das opções de pagamento abaixo:')
+print('0 - À Vista (Dinheiro/Cheque)')
+print('1 - À Vista (Cartão)')
+print('2 - Em até 2 vezes (Cartão)')
+print('3 - Em 3 vezes ou mais (Cartão)')
+opcao_escolhida = int(input('Digite a opção desejada: '))
+if opcao_escolhida == 0:
+    valor_a_pagar = preco_normal * (1 - 10/100)
+    print('O valor a pagar é {:.2f}'.format(valor_a_pagar))
+elif opcao_escolhida == 1:
+    valor_a_pagar = preco_normal * (1 - 5/100)
+    print('O valor a pagar é {:.2f}'.format(valor_a_pagar))
+elif opcao_escolhida == 2:
+    valor_a_pagar = preco_normal
+    print('O valor a pagar é {:.2f}'.format(valor_a_pagar))
+elif opcao_escolhida == 3:
+    valor_a_pagar = preco_normal* (1 + 20/100)
+    print('O valor a pagar é {:.2f}'.format(valor_a_pagar))
+else:
+    print('Opção Inválida!')
