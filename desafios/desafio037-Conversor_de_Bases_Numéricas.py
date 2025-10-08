837b6e6d44ff79d4d9ed16aa2ac8293b3704c35d
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-08 08:27:02 -0300

    Corrigindo o 'desafio037-Conversor_de_Bases_Numericas'

--- desafios/desafio037-Conversor_de_Bases_Numéricas.py
+++ desafios/desafio037-Conversor_de_Bases_Numéricas.py
@@ -2,15 +2,17 @@ print('-#-' * 12)
 print('Conversor de Bases Numéricas')
 print('-#-' * 12)
 num = int(input('Digite um número inteiro: '))
-print('\nEscolha uma das opções abaixo:')
-print('1 - Binário\n2 - Octal\n3 - Hexadecimal\n')
-opcao = int(input('Digite a opção desejada: '))
+print('''Escolha uma das bases para conversão:')
+[ 1 ] - converter para Binário
+[ 2 ] - converter para Octal
+[ 3 ] - converter para Hexadecimal''')
+opcao = int(input('Sua opção: '))
 if opcao == 1:
-    print('O número inteiro {} corresponde ao número {} na base binária.'.format(num, bin(num)))
+    print('O número inteiro {} corresponde ao número {} na base binária.'.format(num, bin(num)[2:]))
 elif opcao == 2:
-    print('O número inteiro {} corresponde ao número {} na base octal.'.format(num, oct(num)))
+    print('O número inteiro {} corresponde ao número {} na base octal.'.format(num, oct(num)[2:]))
 elif opcao == 3:
-    print('O número inteiro {} corresponde ao número {} na base hexadecimal.'.format(num, hex(num)))
+    print('O número inteiro {} corresponde ao número {} na base hexadecimal.'.format(num, hex(num)[2:]))
 else:
-    print('Opção Inválida!')
+    print('Opção Inválida! Tente novamente...')
 
