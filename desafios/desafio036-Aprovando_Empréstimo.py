c00a2388e0ccc30017853d61b3306749cd98b134
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-08 08:03:05 -0300

    Corrigindo o 'desafio036-Aprovando_Emprestimo'

--- desafios/desafio036-Aprovando_Empréstimo.py
+++ desafios/desafio036-Aprovando_Empréstimo.py
@@ -1,12 +1,13 @@
-salario = float(input('Salário: R$ '))
 valor_casa = float(input('Valor da casa: R$ '))
-tempo_anos = int(input('Tempo (anos): '))
-tempo_meses = tempo_anos * 12
-prestacao = valor_casa / tempo_meses
+salario = float(input('Salário: R$ '))
+tempo = int(input('Tempo (anos): '))
+prestacao = valor_casa / (tempo * 12)
+print('Para pegar um empréstimo de R$ {:.2f} em {} anos,'.format(valor_casa, tempo), end='')
+print(' a prestação será de R$ {:.2f}'.format(prestacao))
 if salario * 0.3 > prestacao:
-    print('PRABÉNS! Seu empréstimo foi APROVADO!')
+    print('PARABÉNS! Seu empréstimo foi APROVADO!')
 else:
     print('Infelizmente seu empréstimo foi NEGADO!')
-print(prestacao)
-print(0.3 * salario)
-print(tempo_meses)
+#print(prestacao)
+#print(0.3 * salario)
+#print(tempo_meses)
