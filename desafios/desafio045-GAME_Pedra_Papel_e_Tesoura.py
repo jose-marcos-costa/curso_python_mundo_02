c6c24b8d255be58f1cf4cb28578f6a30a1a7c910
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-08 07:37:07 -0300

    Resolvendo o 'desafio045-GAME_Pedra_Papel_Tesoura'

--- desafios/desafio045-GAME_Pedra_Papel_e_Tesoura.py
+++ desafios/desafio045-GAME_Pedra_Papel_e_Tesoura.py
@@ -1,1 +1,15 @@
-# O arquivo './desafios/desafio045-GAME_Pedra_Papel_e_Tesoura.py' foi criado automaticamente
+from random import choice
+opcoes = ['PEDRA', 'PAPEL', 'TESOURA']
+print('###' * 10)
+print('GAME:  Pedra, Papel, Tesoura')
+print('###' * 10)
+pessoa = str(input('Digite sua jogada: ')).strip().upper()
+computador = choice(opcoes)
+print('A pessoa escolheu: {}'.format(pessoa))
+print('O computador escolheu: {}'.format(computador))
+if pessoa == computador:
+    print('EMPATOU! Jogue novamente...')
+elif (pessoa == 'PEDRA' and computador == 'TESOURA') or (pessoa == 'PAPEL' and computador == 'PEDRA') or (pessoa == 'TESOURA' and computador == 'PAPEL'):
+    print('Parabéns! VOCÊ GANHOU!')
+elif (computador == 'PEDRA' and pessoa == 'TESOURA') or (computador == 'PAPEL' and pessoa == 'PEDRA') or (computador == 'TESOURA' and pessoa == 'PAPEL'):
+    print('HAHAHA! EU GANHEI!')
