e54e1c27b508827b8324edfa1dbe178a73cb8230
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-08 13:42:29 -0300

    Corrigindo o 'desafio043-Indice_de_Massa_Corporal'

--- desafios/desafio043-Índice_de_Massa_Corporal.py
+++ desafios/desafio043-Índice_de_Massa_Corporal.py
@@ -1,9 +1,9 @@
-peso = float(input("Por favor, digite seu peso (Kg): "))
-altura = float(input('Agora, digite sua altura (m): '))
-imc = peso / (altura**2)
+peso = float(input("Por favor, digite seu peso? (Kg): "))
+altura = float(input('Agora, digite sua altura? (m): '))
+imc = peso / (altura ** 2)
 print('IMC = {:.1f}'.format(imc))
 if imc < 18.5:
-    print('Abiaxo do peso')
+    print('Abaixo do peso')
 elif 18.5 <= imc < 25:
     print('Peso ideal')
 elif 25 <= imc < 30:
@@ -12,3 +12,15 @@ elif 30 <= imc <= 40:
     print('Obesidade')
 else:
     print('Obesidade Mórbida')
+"""
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc < 25:
+    print('Peso ideal')
+elif imc < 30:
+    print('Sobrepeso')
+elif imc <= 40:
+    print('Obesidade')
+else:
+    print('Obesidade Mórbida')
+"""
