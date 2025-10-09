2e18ddddbf5715d3b15cc81c49fe81dda3ebd3e1
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-09 14:34:34 -0300

    Adicionando a resolução ao arquivo 'desafio050-Soma_dos_pares' da 'aula13'

--- desafios/desafio050-Soma_dos_pares.py
+++ desafios/desafio050-Soma_dos_pares.py
@@ -1,1 +1,9 @@
-# O arquivo './desafios/desafio050-Soma_dos_pares.py' foi criado automaticamente
+soma = 0
+for i in range(0, 4):
+    numero = int(input('Digite um número inteiro: '))
+    if numero % 2  == 0:
+        soma += numero
+if soma == 0:
+    print('Nenhum número par (diferente de 0) foi digitado!')
+else:
+    print('A soma dos números pares digitados é {}'.format(soma))
