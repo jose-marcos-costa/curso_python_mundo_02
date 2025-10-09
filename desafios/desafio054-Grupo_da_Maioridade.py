0e69166743da1165b8e9e1b3005e5872aa453200
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-09 15:55:18 -0300

    Adicionando a resolução ao arquivo '053-Detector_de_Palindromo' da 'aula13'

--- desafios/desafio054-Grupo_da_Maioridade.py
+++ desafios/desafio054-Grupo_da_Maioridade.py
@@ -1,1 +1,11 @@
-# O arquivo './desafios/desafio054-Grupo_da_Maioridade.py' foi criado automaticamente
+from datetime import date
+cont_maior = 0
+ano_atual = date.today().year
+for i in range (0, 7):
+    ano_nascimento = int(input('Ano de Nascimento (AAAA): '))
+    idade = ano_atual - ano_nascimento
+    print('Sua idade é {}'.format(idade))
+    if idade >= 18:
+        cont_maior += 1
+print('\n{} pessoas já atingiram a maior idade'.format(cont_maior), end='')
+print('\n{} pessoas ainda não atingiram a maioridade!'.format(7 - cont_maior))
