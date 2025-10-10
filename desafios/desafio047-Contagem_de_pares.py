0ae2957cdb0f61b87f19564feaaba4ca8f71bd3a
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-10 09:03:10 -0300

    Corrigindo a resolução do desafio '047-Contagem_de_pares' da 'aula13'

--- desafios/desafio047-Contagem_de_pares.py
+++ desafios/desafio047-Contagem_de_pares.py
@@ -1,4 +1,12 @@
+# Solução do Guanabara - Gasta menos processamento
+for n in range(2, 51, 2):
+    print(n, end=' ')
+print('\nACABOU')
+# Não é o número de linhas que influencia no processamento e sim as repetições. Fez em bateladas (de 2 em 2)
+
+"""
+##### Minha solução #####
 for i in range(1, 51):
     if i % 2 == 0:
         print('O número {:2} é PAR!'.format(i))
-
+"""
