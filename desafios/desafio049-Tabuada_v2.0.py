82a5426bc17f3e67a715628042621d5ee6f51d2d
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-10 15:05:44 -0300

    Corrigindo a resolução do desafio '049-Tabuada_v2.0' da 'aula13'

--- desafios/desafio049-Tabuada_v2.0.py
+++ desafios/desafio049-Tabuada_v2.0.py
@@ -1,4 +1,4 @@
 numero = int(input('Digite um número: '))
 print('{:=^20}'.format(' TABUADA '))
 for i in range(1, 11):
-    print('{:2} X {:2} = {}'.format(i, numero, i * numero))
+    print('{} X {:2} = {}'.format(numero,i, numero*i ))
