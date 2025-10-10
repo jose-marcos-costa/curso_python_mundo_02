a4208c521cf1586e1eae274e7f3ca7753a4b6399
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-10 15:31:48 -0300

    Corrigindo a resolução do desafio '051-Progressao_Aritmetica' da 'aula13'

--- desafios/desafio051-Progressão_Aritmética.py
+++ desafios/desafio051-Progressão_Aritmética.py
@@ -3,4 +3,6 @@ r = int(input('Razão da PA: '))
 n = 10
 for i in range(1, n+1):
     an = a1 + (i-1) * r
-    print('O {}º termo da PA é {}'.format(i, an))
+    #print('O {}º termo da PA é {}'.format(i, an))
+    print('{}'.format(an), end=' → ')
+print('ACABOU')
