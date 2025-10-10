6cb547fab0294f9435c98eb155b0ba9f09e2e212
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-10 09:44:29 -0300

    Refazendo a resolução do desafio '048-Soma_impares_multiplos_de_tres' (v.2.0) da 'aula13' e comparando tempos de execução entre as versões

--- desafios/desafio048-Soma_ímpares_múltiplos_de_três.py
+++ desafios/desafio048-Soma_ímpares_múltiplos_de_três.py
@@ -1,5 +1,38 @@
+"""
+# Primeira versão (sem medição de tempo de execução)
+
 soma = 0
 for i in range(1, 501):
     if i % 2 != 0 and i % 3 == 0:
         soma += i
 print('A soma de todos os números ímpares entre 1 e 500 que são múltiplos de 3 é dada por: {}'.format(soma))
+"""
+
+"""
+# Primeira versão (com medição de tempo de execução)
+
+from time import perf_counter
+tempo_inicial = perf_counter()
+soma = 0
+for i in range(1, 501):
+    if i % 2 != 0 and i % 3 == 0:
+        soma += i
+print('A soma de todos os números ímpares entre 1 e 500 que são múltiplos de 3 é dada por: {}'.format(soma))
+tempo_final = perf_counter()
+tempo_decorrido = tempo_final - tempo_inicial
+print('{}'.format(tempo_decorrido))
+"""
+
+# Segunda versão (com medição de tempo de execução)
+
+from time import perf_counter
+tempo_inicial = perf_counter()
+soma = 0
+for i in range(3, 501, 2):
+    if i % 3 == 0:
+        soma += i
+        #print(i, end=' ')
+print('A soma de todos os números ímpares entre 1 e 500 que são múltiplos de 3 é dada por: {}'.format(soma))
+tempo_final = perf_counter()
+tempo_decorrido = tempo_final - tempo_inicial
+print('{}'.format(tempo_decorrido))
