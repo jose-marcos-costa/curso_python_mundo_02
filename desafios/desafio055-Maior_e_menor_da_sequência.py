7360dcce96c5f18a75225bbee811889b8a497b27
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-12 13:00:05 -0300

    Corrigindo a resolução do desafio '055-Maior_e_menor_da_sequencia' da 'aula13'

--- desafios/desafio055-Maior_e_menor_da_sequência.py
+++ desafios/desafio055-Maior_e_menor_da_sequência.py
@@ -1,9 +1,7 @@
 lista_pesos = []
 for i in range(0, 5):
-    peso = int(input('Peso (Kg): '))
+    peso = float(input('Peso (Kg): '))
     lista_pesos.append(peso)
 lista_pesos.sort()
 print(lista_pesos)
-print('O maior peso é {} Kg e o menor é {} Kg'.format(lista_pesos[4], lista_pesos[0]))
-
-
+print('O maior peso é {:.1f} Kg e o menor é {:.1f} Kg'.format(lista_pesos[4], lista_pesos[0]))
