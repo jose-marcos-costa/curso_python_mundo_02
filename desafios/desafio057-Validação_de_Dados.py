392f1b7470e287d4d3de71874c6bc38870f4de08
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-12 19:22:32 -0300

    Adicionando a resolução ao arquivo '057-Validação_de_Dados' da 'aula14'

--- desafios/desafio057-Validação_de_Dados.py
+++ desafios/desafio057-Validação_de_Dados.py
@@ -1,1 +1,13 @@
-# O arquivo './desafios/desafio057-Validação_de_Dados.py' foi criado automaticamente
+sexo = 'J'
+while sexo != 'M' or sexo != 'F':
+    sexo = str(input('Digite o sexo [M/F]: ')).strip().upper()
+    if sexo == 'M':
+        print('Correto! Sexo MASCULINO digitado!')
+        break
+    elif sexo == 'F':
+        print('Correto! Sexo FEMININO digitado!')
+        break
+    else:
+        print('Incorreto! Digite o sexo novamente!')
+print('FIM')
+
