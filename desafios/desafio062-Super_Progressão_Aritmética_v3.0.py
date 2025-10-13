05f9acc5e35e32223720bbc526c5a7d78d307307
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-13 16:02:29 -0300

    Corrigindo a resolução do desafio 062-Super_Progressao_Aritmética_V3.0-GUANABARA' da 'aula14'

--- desafios/desafio062-Super_Progressão_Aritmética_v3.0.py
+++ desafios/desafio062-Super_Progressão_Aritmética_v3.0.py
@@ -1,15 +1,17 @@
 a1 = int(input('Primeiro termo da PA (a1): '))
 r = int(input('Razão da PA: '))
-aux = a1
 n = 10
 cont = 1
+total = 0
 while n != 0:
+    total = total + n
     while cont <= n:
         an = a1 + (cont - 1) * r
         print('{}'.format(an), end=' → ')
         cont += 1
-    print('ACABOU!')
+    print('PAUSA!')
     n = int(input('Quantos termos a mais você deseja visualizar? '))
     a1 = an + r
     cont = 1
 print('FINISH')
+print('Progressão finalizada com {} termos mostrados.'.format(total))
