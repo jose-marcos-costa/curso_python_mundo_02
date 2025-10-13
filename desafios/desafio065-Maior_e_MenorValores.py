891c86f9468808f8b9cc298a6a62e79e78cee491
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-13 14:28:57 -0300

    Adicionando a resolução ao arquivo '065-Maior_e_MenorValores' da 'aula14'

--- desafios/desafio065-Maior_e_MenorValores.py
+++ desafios/desafio065-Maior_e_MenorValores.py
@@ -1,1 +1,23 @@
-# O arquivo './desafios/desafio065-Maior_e_MenorValores.py' foi criado automaticamente
+cont = 0
+soma = 0
+media = 0
+maior = 0
+menor = 0
+opcao = 'S'
+while opcao != 'N':
+    cont += 1
+    numero = int(input('\nDigite um número inteiro: '))
+    soma += numero
+    if cont == 1:
+        maior = numero
+        menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+    opcao = str(input('\nDeseja continuar [S/N]: ')).strip().upper()
+media = soma / cont
+print('Média = {:.2f}'.format(media), end='\n')
+print('Maior = {}'.format(maior), end='\n')
+print('Menor = {}'.format(menor))
