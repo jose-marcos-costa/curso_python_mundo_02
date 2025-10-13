ce46a015fa3f99d1bdf19aac91fdc0c6e3c70013
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-13 09:19:10 -0300

    Adicionando a resolução ao arquivo '058-Jogo_da_Adivinhação_v2.0' da 'aula14'

--- desafios/desafio058-Jogo_da_Adivinhação_v2.0.py
+++ desafios/desafio058-Jogo_da_Adivinhação_v2.0.py
@@ -1,1 +1,21 @@
-# O arquivo './desafios/desafio058-Jogo_da_Adivinha��o_v2.0.py' foi criado automaticamente
+from time import sleep
+from random import randint
+cont = 0
+numero_jogador = 11
+numero_computador = randint(0, 10)
+print('PENSANDO',end='')
+sleep(0.5)
+print('*', end='')
+sleep(0.5)
+print('*', end='')
+sleep(0.5)
+print('*')
+sleep(0.5)
+print('PENSEI!\n')
+while numero_jogador != numero_computador:
+    cont += 1
+    numero_jogador = int(input('Digite um número (0 a 10): '))
+    if numero_jogador == numero_computador:
+        print('Você ACERTOU! O número escolhido foi {} e você precisou de {} palpite(s) para acertar'.format(numero_computador, cont))
+    else:
+        print('Tente novamente!')
