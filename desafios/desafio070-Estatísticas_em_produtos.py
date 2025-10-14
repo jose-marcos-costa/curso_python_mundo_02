836f8de732c4dd7d2f8be91e2df4ab9b63e21887
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-14 17:44:37 -0300

    Corrigindo a resolução do desafio '070-Estatisticas_em_produtos' da 'aula15'

--- desafios/desafio070-Estatísticas_em_produtos.py
+++ desafios/desafio070-Estatísticas_em_produtos.py
@@ -1,28 +1,23 @@
-from functools import total_ordering
-
 nome_mais_barato = ''
 preco_mais_barato = 0.0
 cont_mais_1000 = 0
-controle = 'S'
 total = 0.0
 cont = 0
-while controle == 'S':
+while True:
     nome = str(input('Digite o nome do PRODUTO: ')).strip()
-    preco = float(input('Digite o preço do PRODUTO: '))
+    preco = float(input('Digite o preço do PRODUTO: R$ '))
     total += preco
-    if cont == 0:
+    if cont == 0 or preco < preco_mais_barato:
         preco_mais_barato = preco
         nome_mais_barato = nome
-    else:
-        if preco < preco_mais_barato:
-            preco_mais_barato = preco
-            nome_mais_barato = nome
-        if preco > 1000.0:
-            cont_mais_1000 += 1
+    if preco > 1000.0:
+        cont_mais_1000 += 1
     controle = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     while controle not in 'SN':
         controle = str(input('Opção Inválida! Deseja continuar? [S/N] ')).strip().upper()[0]
+    if controle == 'N':
+        break
     cont += 1
-print(f'O total gasto foi de R$ {total}')
+print(f'O total gasto foi de R$ {total:.2f}')
 print(f'{cont_mais_1000} produtos custam mais de R$ 1000')
-print(f'O produto mais barato é {nome_mais_barato}')
+print(f'O produto mais barato é {nome_mais_barato} custando R$ {preco_mais_barato:.2f}')
