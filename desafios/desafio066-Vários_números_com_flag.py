da43248fa98835feb26c7ddd10d3cc3b47068e0d
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-14 17:02:25 -0300

    Corrigindo a resolução do desafio '066-Varios_numeros_com_flag' da 'aula15'

--- desafios/desafio066-Vários_números_com_flag.py
+++ desafios/desafio066-Vários_números_com_flag.py
@@ -1,10 +1,9 @@
-cont = soma = numero = 0
-while numero != 999:
+cont = soma = 0
+while True:
     numero = int(input('Digite um número [999 para sair]: '))
     if numero == 999:
         print(f'SAINDO...')
         break
-    else:
-        soma += numero
-        cont += 1
+    soma += numero
+    cont += 1
 print(f'Foram digitados {cont} e a soma entre eles é {soma}')
