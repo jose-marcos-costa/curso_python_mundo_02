c8e4cb42ed60cf5cff586cf84cb46a6b256b06c5
jose-marcos-costa <josemarcoscosta@live.com>
2025-10-14 09:28:08 -0300

    Adicionando a resolução ao arquivo '067-Tabuada_v3.0' da 'aula15'

--- desafios/desafio067-Tabuada_v3.0.py
+++ desafios/desafio067-Tabuada_v3.0.py
@@ -1,1 +1,12 @@
-# O arquivo './desafios/desafio067-Tabuada_v3.0.py' foi criado automaticamente
+numero = 0
+while not numero < 0:
+    numero = int(input('Digite um número: '))
+    if numero < 0:
+        print(f'Finalizando...')
+        break
+    else:
+        for i in range (1, 11):
+            resultado = numero * i
+            print(f'{numero} x {i:2} = {resultado}')
+    print('-=' * 10)
+
